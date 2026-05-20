057acd82278804fea4ff641d0098d3d413bf3d7c
Hambat E. <hambateverett@gmail.com>
2026-05-19 19:33:17 -0700

    Update turtleprinter.lua

--- turtleprinter.lua
+++ turtleprinter.lua
@@ -3,20 +3,26 @@ term.clear()
 term.setCursorPos(1,1)
 
 local layers = {
-  "u3f3f3f3f3rf3f3f3f3f3rf3f3f3f3f3rf3f3f3f3fr",
-  "u3f3f3f3f3rf3f3f3f3f3rf3f3f3f3f3rf3f3f3f3fr",
-  "u3f3f3f3f3rf3f3f3f3f3rf3f3f3f3f3rf3f3f3f3fr"
+  "6f6f6f6f6f6rf6rf6f6f6f6f6lf6lf6f6f6f6f6rf6rf6f6f6f6f6lf6lf6f6f6f6f6"
 }
 
-if character == "u" then turtle.up()
-elseif character == "f" then turtle.forward()
-elseif character == "r" then turtle.turnRight()
-elseif character == "l" then turtle.turnLeft()
-elseif character == "d" then turtle.down()
-elseif character == "1" then turtle.place()
-elseif character == "2" then turtle.placeUp()
-elseif character == "3" then turtle.placeDown()
-elseif character == "4" then turtle.dig()
-elseif character == "5" then turtle.digUp()
-elseif character == "6" then turtle.digDown()
-else print("Invalid instruction. ("..character.." at "..i..")") end
+for layerIndex, layerStr in ipairs(layers) do
+  for i = 1, #layerStr do
+    local character = layerStr:sub(i, i)
+    turtle.select(1)
+
+    if character == "u" then turtle.up()
+    elseif character == "f" then turtle.forward()
+    elseif character == "b" then turtle.back()
+    elseif character == "r" then turtle.turnRight()
+    elseif character == "l" then turtle.turnLeft()
+    elseif character == "d" then turtle.down()
+    elseif character == "1" then turtle.place()
+    elseif character == "2" then turtle.placeUp()
+    elseif character == "3" then turtle.placeDown()
+    elseif character == "4" then turtle.dig()
+    elseif character == "5" then turtle.digUp()
+    elseif character == "6" then turtle.digDown()
+    else print("Invalid instruction. ("..character.." at "..i..")") end
+  end
+end
